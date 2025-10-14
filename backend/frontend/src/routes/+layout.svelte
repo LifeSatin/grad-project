7f69263811904e60f9a147658989660ffd3c538c
LifeSatin <161478370+LifeSatin@users.noreply.github.com>
2025-10-14 16:46:33 +0900

    frontend

---
+++ backend/frontend/src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<nav>
+    검색창
+    알림함
+    <a href="/signup">회원가입</a>
+    <a href="/login">로그인</a>
+</nav>
+
+<nav>
+    <ul>
+        <li><a href="/notice">공지사항</a></li>
+        <li><a href="/question">질문 게시판</a></li>
+        <li><a href="/discuss">토론 게시판 전체 목록</a></li>
+        <li>즐겨찾기</li>
+        <li><a href="/wiki">위키</a></li>
+    </ul>
+</nav>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children?.()}

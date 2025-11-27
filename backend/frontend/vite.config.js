7ea3b67b2b194a25909394222a78b39c5401d40c
LifeSatin <161478370+LifeSatin@users.noreply.github.com>
2025-11-27 14:55:25 +0900

    nip.io

--- backend/frontend/vite.config.js
+++ backend/frontend/vite.config.js
@@ -2,6 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
 	server.allowedHosts: ["3.27.115.22.nip.io"]
 });

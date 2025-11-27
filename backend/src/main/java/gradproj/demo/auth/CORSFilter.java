97de3b1964b226cc89b76034f1fd60420451dd26
LifeSatin <161478370+LifeSatin@users.noreply.github.com>
2025-11-28 07:38:55 +0900

    https

--- backend/src/main/java/gradproj/demo/auth/CORSFilter.java
+++ backend/src/main/java/gradproj/demo/auth/CORSFilter.java
@@ -9,7 +9,7 @@ public class CORSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "https://3.27.115.22.nip.io");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");

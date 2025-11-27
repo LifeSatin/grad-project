0108fcc3c959ea0ef55769c4a81030740bceab47
LifeSatin <161478370+LifeSatin@users.noreply.github.com>
2025-11-27 10:11:33 +0900

    cors 4

--- backend/src/main/java/gradproj/demo/config/WebConfig.java
+++ backend/src/main/java/gradproj/demo/config/WebConfig.java
@@ -26,7 +26,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:5173")
+                .allowedOrigins("http://localhost:8080", "http://localhost:5173", "http://frontend:5173")
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST");
     }

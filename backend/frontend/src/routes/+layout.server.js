3e8c4a5f1875489cd8e00902d6db5ffb93266c10
LifeSatin <161478370+LifeSatin@users.noreply.github.com>
2025-11-27 14:13:25 +0900

    login 1

--- backend/frontend/src/routes/+layout.server.js
+++ backend/frontend/src/routes/+layout.server.js
@@ -1,6 +1,7 @@
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({ cookies }) {
   const token = cookies.get('token') ?? null;
+  console.log(token);
   if (token != null) {
     const res = await fetch(`http://3.27.115.22:8080/user?token=${token}`);
     const item = await res.json();

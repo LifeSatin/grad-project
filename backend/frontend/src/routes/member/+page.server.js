a1392878630e959a74b0ff304fc76cf30cf8721a
LifeSatin <161478370+LifeSatin@users.noreply.github.com>
2025-11-27 10:03:38 +0900

    cors 3

--- backend/frontend/src/routes/member/+page.server.js
+++ backend/frontend/src/routes/member/+page.server.js
@@ -9,9 +9,10 @@ export async function load({ fetch, params, cookies }) {
     const item = await res.json();
     console.log(item);
 
-    const comres = await fetch(`http://backend:8080/member/comments?memberId=${item.id}`,  headers: {
+    const comres = await fetch(`http://backend:8080/member/comments?memberId=${item.id}`,  {
+        headers: {
               "Access-Control-Allow-Origin": "http://backend:8080",
-     })
+     }})
     console.log("comres");
     console.log(comres);
     const comlist = await comres.json();

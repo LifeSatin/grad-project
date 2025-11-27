7605323580152df71c87745f19fd4d2edbf96aef
LifeSatin <161478370+LifeSatin@users.noreply.github.com>
2025-11-28 06:11:35 +0900

    https

--- backend/frontend/vite.config.js
+++ backend/frontend/vite.config.js
@@ -1,9 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
+	    proxy: {},
 	    allowedHosts: ["3.27.115.22.nip.io"],
+	    plugins: [sveltekit(), mkcert()]
 	}
 });

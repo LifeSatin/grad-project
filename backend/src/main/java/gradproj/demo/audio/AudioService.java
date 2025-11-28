f45741c9d4a1b33af907dd818963f3fc8612ca7c
LifeSatin <161478370+LifeSatin@users.noreply.github.com>
2025-11-28 11:49:38 +0900

    mp3

--- backend/src/main/java/gradproj/demo/audio/AudioService.java
+++ backend/src/main/java/gradproj/demo/audio/AudioService.java
@@ -7,6 +7,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Service
 @Slf4j
@@ -15,12 +19,21 @@ public class AudioService {
 
     public void uploadFile(String fileId, MultipartFile file) {
         try {
-            if (!file.isEmpty()) {file.transferTo(new File("/files/" + fileId + ".mp3"));}
+            if (file.isEmpty()) {
+                log.info("file is empty");
+            } else {
+                file.transferTo(new File("/files/" + fileId + ".mp3"));
+            }
         } catch (Exception e) {
             log.info("file upload error");
             log.info(e.getMessage());
             return;
         }
         log.info("file upload success");
+        try {
+            log.info("exists: " + Files.exists(Path.of(new URI("/files/" + fileId + ".mp3"))));
+        } catch (URISyntaxException e) {
+            log.info("URI wrong");
+        }
     }
 }
